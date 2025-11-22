2a42e56ca046ba38bb4aaed2036c05ec35a8ca70
Nicolas Regez <reg@algosys.ch>
2025-11-22 15:59:21 +0100

    Initial version of labels and scales

--- src/main/java/org/jfree/chord/plot/ChordDiagram.java
+++ src/main/java/org/jfree/chord/plot/ChordDiagram.java
@@ -4,11 +4,14 @@ import org.jfree.chart.entity.EntityCollection;
 import org.jfree.chart.plot.Plot;
 import org.jfree.chart.plot.PlotRenderingInfo;
 import org.jfree.chart.plot.PlotState;
+import org.jfree.chart.text.TextUtils;
+import org.jfree.chart.ui.TextAnchor;
 import org.jfree.chord.data.ChordDataset;
 
 import java.awt.*;
 import java.awt.geom.Arc2D;
 import java.awt.geom.Area;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
@@ -58,7 +61,7 @@ public class ChordDiagram extends Plot {
             entities = info.getOwner().getEntityCollection();
         }
 
-        double r = Math.min(area.getWidth(), area.getHeight()) / 2 * 0.8;
+        float r = (float) (Math.min(area.getWidth(), area.getHeight()) / 2 * 0.8);
         double cx = area.getCenterX();
         double cy = area.getCenterY();
 
@@ -71,26 +74,28 @@ public class ChordDiagram extends Plot {
          * Ordering convention: for each group, draw outflow first, start with the
          * biggest value, when outflow
          */
-        
+
         double startAngle = 0.0;
         for (String key : dataset.getKeys()) {
             double keyFlow = dataset.getTotalOutflux(key) + dataset.getTotalInflux(key);
             double angle = (keyFlow / totalFlow) * 2 * Math.PI; // arc length in radians
             double endAngle = startAngle + angle;
 
+            var spacing = 2.0 * Math.PI * 0.025 / dataset.getKeys().size();
+
             // Outer arc (full radius)
             Arc2D outerArc = new Arc2D.Double(
                     cx - r, cy - r, 2 * r, 2 * r,
-                    Math.toDegrees(startAngle),
-                    Math.toDegrees(angle),
+                    Math.toDegrees(startAngle + spacing),
+                    Math.toDegrees(angle - spacing),
                     Arc2D.PIE);
 
             // Inner arc (smaller radius to mask center)
             double innerR = r * 0.95; // keep 5% thickness
             Arc2D innerArc = new Arc2D.Double(
                     cx - innerR, cy - innerR, 2 * innerR, 2 * innerR,
-                    Math.toDegrees(startAngle),
-                    Math.toDegrees(angle),
+                    Math.toDegrees(startAngle + spacing),
+                    Math.toDegrees(angle - spacing),
                     Arc2D.PIE);
 
             // Subtract inner from outer
@@ -113,6 +118,34 @@ public class ChordDiagram extends Plot {
             }
 
             // TODO add a separate entity for the connector and have a tooltip to show the values
+
+            {
+                g2.setPaint(color);
+                var a = 0.5 * (startAngle + endAngle);
+
+                var x = (float) (cx + 1.15 * r * Math.cos(a));
+                var y = (float) (cy - 1.15 * r * Math.sin(a));
+                TextUtils.drawRotatedString(key, g2, x, y, TextAnchor.CENTER, 0.5 * Math.PI - a, TextAnchor.CENTER);
+            }
+
+            {
+                g2.setColor(Color.BLACK);
+                var circle = new Arc2D.Double(
+                        cx - r, cy - r, 2 * r, 2 * r,
+                        Math.toDegrees(startAngle + spacing),
+                        Math.toDegrees(angle - spacing),
+                        Arc2D.OPEN);
+                g2.draw(circle);
+                for (int i = 0; i <= 5; i++) {
+                    var a = startAngle + spacing + 0.2 * i * (endAngle - startAngle - spacing);
+                    var x1 = (float) (cx + r * Math.cos(a));
+                    var y1 = (float) (cy - r * Math.sin(a));
+                    var x2 = x1 + 0.03 * r * Math.cos(a);
+                    var y2 = y1 - 0.03 * r * Math.sin(a);
+                    var line = new Line2D.Double(x1, y1, x2, y2);
+                    g2.draw(line);
+                }
+            }
 
             startAngle = endAngle;
         }
